78342a6fd92f2fbda0d9204879907d36adc1ea92
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2009-12-28 15:16:07 -0700

    Added a visitor

--- taskModel.py
+++ taskModel.py
@@ -2,6 +2,8 @@
 #~ Imports 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+import itertools
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~ Definitions 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,15 +22,14 @@ class TaskAtom(object):
 class TaskTextAtom(TaskAtom):
     indent = ''
     text = None
-    tags = ()
-    children = None
+    tags = ()       # a list of tags
+    children = ()   # a list of task atoms
 
     info = None
     def __init__(self, info):
         self.indent = info.pop('indent', '')
         self.text = info.pop('text')
-        tags = info.pop('tags', None)
-        if tags: self.tags = tags
+        self.tags = info.pop('tags', [])
         if info: self.info = info
         self.init()
 
@@ -43,6 +44,14 @@ class TaskTextAtom(TaskAtom):
         indent = ''.join(sorted(indent))
         self._indent = indent
     indent = property(getIndent, setIndent)
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def __getitem__(self, tag):
+        return [e for e in self.tags if e.tag == tag]
+    def __contains__(self, tag):
+        return bool(self[tag])
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -100,4 +109,28 @@ class TaskTag(TaskAtom):
     def isTag(self): return True
     def accept(self, v, *args, **kw): 
         return v.visitTaskTag(self, *args, **kw)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Visitor
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TaskAtomVisitor(object):
+    def visit(self, taskAtom):
+        return taskAtom.accept(self)
+    def _visitIter(self, atomIter, *args, **kw):
+        if atomIter is None: 
+            return None
+        return [ta.accept(self, *args, **kw) for ta in atomIter]
+
+    def visitTaskProject(self, project):
+        self._visitIter(project, project)
+        self._visitIter(project.tags, project)
+    def visitTaskItem(self, task, parent=None):
+        self._visitIter(task, task)
+        self._visitIter(task.tags, task)
+    def visitTaskNote(self, note, parent=None):
+        self._visitIter(note, note)
+        self._visitIter(note.tags, note)
+    def visitTaskTag(self, tag, parent=None):
+        pass
 
